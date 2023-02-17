0623e2e8dba980e9dfb0d51e180b6922556c0809
DrSchiz <sergeyts04@yandex.ru>
2023-02-17 12:58:42 +0300

    Код исправлен

--- Project2.py
+++ Project2.py
@@ -1,21 +1,26 @@
 import calendar
 
-month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-odd = 172
-even = 168
-leap = 165
-notLeap = 154
 year = int(input("Введите год: "))
+if (calendar.isleap(year)):
+    feb = 29
+else:
+    feb = 28
+month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+days = [31, feb, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
+
+def sumDigits(n):
+    sum = 0
+    while n!=0:
+        sum += n%10
+        n=n//10
+    return sum
+
+day = 0
 
 for i in range(12):
-    if (i)%2 == 0:
-        print(f"{month[i]}: {odd}")
-    else:
-        if i == 1:
-            if (calendar.isleap(year)):
-                print(f"{month[i]}: {leap}")
-            else:
-                print(f"{month[i]}: {notLeap}")
-
-        else:
-            print(f"{month[i]}: {even}")
+    count = 0
+    for a in range(days[i]+1):
+        count += sumDigits(a)
+    day += count
+    print(f"{month[i]}: {count}")
+print(f"Количество дней в году: {day}")
